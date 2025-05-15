23e7ee1f29910d1a805f4d3c48c9ab00c2eb2d13
william dutton <will.dutt@gmail.com>
2025-05-16 09:20:51 +1000

    feature: CSRF only activated when used or already in session

--- ckan/views/util.py
+++ ckan/views/util.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask_wtf.csrf import generate_csrf
 
 import ckan.lib.base as base
 from ckan.lib.helpers import helper_functions as h
-from ckan.common import _, request
+from ckan.common import _, request, g
 from ckan.types import Response
 
 util = Blueprint(u'util', __name__)
@@ -32,6 +33,16 @@ def primer() -> str:
     return base.render(u'development/primer.html')
 
 
+def csrf_input() -> Response:
+    """ Generate a CSRF token and return it in a JSON response for XHR POST requests.
+    Note: CORS protects this endpoint cross domain in XHR/Fetch context"""
+    return jsonify({
+        "name": g.csrf_field_name,
+        "value": generate_csrf()
+    })
+
+
 util.add_url_rule(
     u'/util/redirect', view_func=internal_redirect, methods=(u'GET', u'POST',))
 util.add_url_rule(u'/testing/primer', view_func=primer)
+util.add_url_rule(u'/csrf-input', view_func=csrf_input, methods=(u'GET',))
